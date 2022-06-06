ecfd71943486f681d3268496d40df086b32adb09
4bnuk <vaggelis_tsili@yahoo.gr>
2022-06-06 22:18:22 +0300

    Make people view functional

--- src/views/People.vue
+++ src/views/People.vue
@@ -1,7 +1,11 @@
 <script setup>
-import { ref } from 'vue'
-import { onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import api from '../api.js'
+
+const profilePath = computed(() => profile_path => {
+  if (profile_path) return "https://image.tmdb.org/t/p/w500/" + profile_path
+  else return 'https://via.placeholder.com/300x450'
+})
 
 // onMounted(() => {
 //   fetch(`https://api.themoviedb.org/3/person/popular?api_key=${api.key}`)
@@ -1340,9 +1344,26 @@ const people = ref([
 </script>
 
 <template>
-  <h1>Popular People</h1>
-  <div v-for="person in people" :key="person.id">
-    {{ person.name }}
+  <div class="container mx-auto px-4 pt-16">
+    <div class="popular-movies pb-16">
+      <h2 class="uppercase tracking-wider text-amber-300 text-xl font-bold">Popular People</h2>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+        <div v-for="person in people" :key="person.id" class="mt-8">
+          <router-link :to="`/person/${person.id}`">
+            <img :src="profilePath(person.profile_path)" :alt="`Picture of ${person.name}`"
+              class="hover:opacity-75 transition ease-in-out duration-150">
+          </router-link>
+          <div class="mt-2">
+            <router-link :to="`/person/${person.id}`" class="text-lg mt-2 hover:text-gray-300">
+              {{ person.name }}
+            </router-link>
+            <div class="flex items-center text-gray-400 text-sm mt-1">
+              <div class="text-gray-400 text-sm">{{ person.known_for_department }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
