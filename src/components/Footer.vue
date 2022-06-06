639064154e67b80b67a9bf1035521f6b589f732d
4bnuk <vaggelis_tsili@yahoo.gr>
2022-06-06 22:28:18 +0300

    Change footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,14 @@
 </script>
 
 <template>
-  <p>Created by me</p>
-
+  <footer class="border border-t border-gray-800">
+    <div class="container mx-auto p-4">
+      <p>Created by Me</p>
+      <p>This product uses the<a href="https://www.themoviedb.org/documentation/api"> TMDB API</a> but is not endorsed
+        or
+        certified by TMDB</p>
+    </div>
+  </footer>
 </template>
 
 <style>
