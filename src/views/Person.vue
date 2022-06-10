8a686356290cb9db1a74ec17fa3f054ff4dbd331
4bnuk <vaggelis_tsili@yahoo.gr>
2022-06-10 20:44:45 +0300

    Calculate and show the age of person

--- src/views/Person.vue
+++ src/views/Person.vue
@@ -10,6 +10,7 @@ const props = defineProps({
 })
 
 const profilePath = computed(() => "https://image.tmdb.org/t/p/w400/" + person.value.profile_path)
+const personAge = computed(() => Math.floor((new Date() - new Date(person.value.birthday).getTime()) / 3.15576e+10))
 
 const formatDate = computed(() => date => new Date(date).toLocaleDateString('el-GR'))
 
@@ -123,7 +124,7 @@ const renderTemplate = ref(false)
               <path
                 d="M448 384c-28.02 0-31.26-32-74.5-32-43.43 0-46.825 32-74.75 32-27.695 0-31.454-32-74.75-32-42.842 0-47.218 32-74.5 32-28.148 0-31.202-32-74.75-32-43.547 0-46.653 32-74.75 32v-80c0-26.5 21.5-48 48-48h16V112h64v144h64V112h64v144h64V112h64v144h16c26.5 0 48 21.5 48 48v80zm0 128H0v-96c43.356 0 46.767-32 74.75-32 27.951 0 31.253 32 74.75 32 42.843 0 47.217-32 74.5-32 28.148 0 31.201 32 74.75 32 43.357 0 46.767-32 74.75-32 27.488 0 31.252 32 74.5 32v96zM96 96c-17.75 0-32-14.25-32-32 0-31 32-23 32-64 12 0 32 29.5 32 56s-14.25 40-32 40zm128 0c-17.75 0-32-14.25-32-32 0-31 32-23 32-64 12 0 32 29.5 32 56s-14.25 40-32 40zm128 0c-17.75 0-32-14.25-32-32 0-31 32-23 32-64 12 0 32 29.5 32 56s-14.25 40-32 40z" />
             </svg>
-            <span class="ml-2">{{ formatDate(person.birthday) }} ({{ person.age }} years old) in {{
+            <span class="ml-2">{{ formatDate(person.birthday) }} ( {{ personAge }} years old ) in {{
                 person.place_of_birth
             }}</span>
           </div>
