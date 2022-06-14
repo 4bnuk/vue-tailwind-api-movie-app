e9442a8123d0500a79876ec9f8d77812ad8cff41
4bnuk <vaggelis_tsili@yahoo.gr>
2022-06-14 22:35:17 +0300

    Change footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,18 +11,19 @@ function toggleMode() {
 </script>
 
 <template>
-  <footer class="border-t dark:border-gray-800 border-gray-400">
-    <div class="flex justify-between container place-items-center mx-auto p-4">
+  <footer class="border-t dark:border-gray-800 border-gray-400 py-3">
+    <div class="flex flex-col-reverse md:flex-row justify-between container place-items-center mx-auto p-4">
       <div>
         <p>Created by Me</p>
         <p>This product uses the<a href="https://www.themoviedb.org/documentation/api"> TMDB API</a> but is not endorsed
           or
           certified by TMDB</p>
       </div>
-      <div class="flex space-x-4">
+      <div class="flex space-x-4 justify-center mb-10 md:mb-0">
         <span v-if="darkMode">Light Mode:</span>
         <span v-else>Dark Mode:</span>
-        <svg v-if="darkMode" @click="toggleMode" class="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <svg v-if="darkMode" @click="toggleMode" class="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24"
+          stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round"
             d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
