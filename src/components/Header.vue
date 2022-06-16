41bdc23adbb775dcf854da7e70fabe4e6806d441
4bnuk <vaggelis_tsili@yahoo.gr>
2022-06-16 22:07:14 +0300

    Change spinner color in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -90,7 +90,7 @@ function callApi() {
             <input @focus="searchIsOpen = true" @keydown="searchIsOpen = true" v-model="searchText"
               @input="debounceSearch" type="text"
               class="dark:bg-gray-800 dark:border-0 border bg-slate-200 border-gray-400 text-sm rounded-full w-full md:w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
-              placeholder="Search...">
+              placeholder="Search a movie...">
             <div class="absolute top-0">
               <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
                 <path class="heroicon-ui"
@@ -98,7 +98,7 @@ function callApi() {
               </svg>
             </div>
 
-            <svg v-show="toggleSpinner" role="status" class="absolute top-1 right-2 w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            <svg v-show="toggleSpinner" role="status" class="absolute top-1 right-2 w-5 h-5 text-gray-400 fill-red-500 animate-spin dark:text-gray-600 dark:fill-amber-300"
               viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
