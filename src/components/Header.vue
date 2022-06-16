e558c5b5bc6d0334dcfb4b3fd29e87020bf75b81
4bnuk <vaggelis_tsili@yahoo.gr>
2022-06-16 21:41:45 +0300

    Add loading spinner in search on header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,6 +7,8 @@ const target = ref(null)
 const searchIsOpen = ref(false)
 const debounce = ref(null)
 const mobileMenuIsOpen = ref(false)
+const toggleSpinner = ref(false)
+const noResults = ref(false)
 
 onMounted(() => {
   document.addEventListener('click', (e) => {
@@ -21,6 +23,7 @@ function toggleMenu() {
 }
 
 function debounceSearch() {
+  noResults.value = false
   clearTimeout(debounce.value)
   debounce.value = setTimeout(() => {
     searchResults.value = []
@@ -31,10 +34,13 @@ function debounceSearch() {
 }
 
 function callApi() {
+  toggleSpinner.value = true
   fetch(`https://api.themoviedb.org/3/search/movie?query=${searchText.value}&api_key=${import.meta.env.VITE_API_KEY}`)
     .then(response => response.json())
     .then(data => {
+      if(!data.results.length) noResults.value = true
       searchResults.value = data.results.slice(0, 5)
+      toggleSpinner.value = false
       console.log('searchResults', searchResults.value)
     });
 
@@ -65,13 +71,16 @@ function callApi() {
       <ul class="dark:bg-gray-900 bg-slate-200 text-xl md:flex md:items-center md:px-0 px-10 md:pb-0 pb-10 md:static absolute 
         md:w-auto w-full top-14 duration-300 ease-in" :class="[mobileMenuIsOpen ? 'left-0' : 'left-[-100%]']">
         <li class="md:mx-4 md:my-0 my-6">
-          <router-link @click="toggleMenu" class="dark:hover:text-gray-300 hover:text-gray-500" to="/">Movies</router-link>
+          <router-link @click="toggleMenu" class="dark:hover:text-gray-300 hover:text-gray-500" to="/">Movies
+          </router-link>
         </li>
         <li class="md:mx-4 md:my-0 my-6">
-          <router-link @click="toggleMenu" class="dark:hover:text-gray-300 hover:text-gray-500" to="/tvShows">Tv Shows</router-link>
+          <router-link @click="toggleMenu" class="dark:hover:text-gray-300 hover:text-gray-500" to="/tvShows">Tv Shows
+          </router-link>
         </li>
         <li class="md:mx-4 md:my-0 my-6">
-          <router-link @click="toggleMenu" class="dark:hover:text-gray-300 hover:text-gray-500" to="/people">People</router-link>
+          <router-link @click="toggleMenu" class="dark:hover:text-gray-300 hover:text-gray-500" to="/people">People
+          </router-link>
         </li>
       </ul>
       <div class="ml-auto md:flex md:items-center md:px-0 px-10 md:pb-0 pb-10 md:static absolute
@@ -89,12 +98,21 @@ function callApi() {
               </svg>
             </div>
 
-            <!-- <div class="absolute spinner top-0 right-0 mr-4 mt-3"></div> -->
+            <svg v-show="toggleSpinner" role="status" class="absolute top-1 right-2 w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor" />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill" />
+            </svg>
 
             <div v-show="searchText.length > 2 && searchIsOpen"
               class="z-50 absolute dark:bg-gray-800 dark:border-0 border border-gray-400 bg-slate-200 text-sm rounded w-full md:w-64 mt-5">
-              <ul v-if="searchResults.length">
-                <li v-for="searchResult in searchResults" :key="searchResult.id" class="border-b dark:border-gray-700 border-gray-400">
+              <ul v-show="searchResults.length">
+                <li v-for="searchResult in searchResults" :key="searchResult.id"
+                  class="border-b dark:border-gray-700 border-gray-400">
                   <router-link @click="searchIsOpen = false" :to="`/movie/${searchResult.id}`"
                     class="dark:hover:bg-gray-700 hover:bg-gray-500 hover:text-white px-3 py-3 flex items-center transition ease-in-out duration-150">
                     <img v-if="searchResult.poster_path"
@@ -104,7 +122,7 @@ function callApi() {
                   </router-link>
                 </li>
               </ul>
-              <div v-else class="px-3 py-3">No results for "{{ searchText }}"</div>
+              <div v-show="noResults" class="px-3 py-3">No results for "{{ searchText }}"</div>
             </div>
           </div>
         </div>
@@ -113,5 +131,5 @@ function callApi() {
   </nav>
 </template>
 
-<style>
+<style scoped>
 </style>
