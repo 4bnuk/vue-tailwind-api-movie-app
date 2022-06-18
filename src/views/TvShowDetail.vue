cb90bad5c28d4e69c5590848b75b6effc1d470fd
4bnuk <vaggelis_tsili@yahoo.gr>
2022-06-18 17:52:26 +0300

    Center main image in tv show detail

--- src/views/TvShowDetail.vue
+++ src/views/TvShowDetail.vue
@@ -50,7 +50,7 @@ const showModal = ref(false)
   <div v-if="renderTemplate">
     <div class="tv-show-info border-b dark:border-gray-800 border-gray-400">
       <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
-        <div class="flex-none">
+        <div class="flex justify-center md:flex-none">
           <img :src="posterPath" :alt="`Poster of the ${tvShow.name} TV Show`" class="w-64 lg:w-96">
         </div>
         <div class="md:ml-24">
