4a43e81ff1db44f9749049df8b5b2adb173e916f
4bnuk <vaggelis_tsili@yahoo.gr>
2022-06-23 21:20:28 +0300

    Change size of images on mobile view

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -51,7 +51,7 @@ const showModal = ref(false)
     <div class="movie-info border-b dark:border-gray-800 border-gray-400">
       <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
         <div class="flex justify-center md:flex-none">
-          <img :src="posterPath" :alt="`Poster of the ${movie.title} movie`" class="w-64 lg:w-96">
+          <img :src="posterPath" :alt="`Poster of the ${movie.title} movie`" class="w-96">
         </div>
         <div class="md:ml-24">
           <h2 class="text-4xl mt-4 md:mt-0 font-semibold dark:text-amber-300 text-red-500">{{ movie.title }}</h2>
